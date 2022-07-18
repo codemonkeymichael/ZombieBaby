be664e0719687a88bbfbe28fec7d02471964a85b
Michael Strange <mike@michaelstrange.net>
2022-07-18 10:55:43 -0700

    Playlist WIP

--- Playlists/Defcon2.cs
+++ Playlists/Defcon2.cs
@@ -1,10 +1,20 @@
-﻿namespace ZombieBaby.Playlists;
+﻿using ZombieBaby.Utilities;
+
+namespace ZombieBaby.Playlists;
 
 public static class Defcon2
 { 
     public static void Defcon2A()
     {
+
+
         Console.WriteLine("Playlists Awake1()");
+
+        Status.StopStatus2 = false;
+        Status.CurrentStatus = 2;
+        Thread status2 = new Thread(() => Status.Status2(30));
+        status2.Start();
+
         Playlists.Eyes.Open();
     }
 }
--- Playlists/Head.cs
+++ Playlists/Head.cs
@@ -29,12 +29,29 @@ public static class Head
     {
         for (int i = 0; i < count; i++)
         {
+            Movement.Head.LeftHalf();
+            Thread.Sleep(1000);
+            Movement.Head.Center();
+            Thread.Sleep(1000);
+            Movement.Head.RightHalf();
+            Thread.Sleep(1000);
+            Movement.Head.Center();
+            Thread.Sleep(1000);
+            Movement.Head.Release();
+            Thread.Sleep(1000);
+        }
+    }
+
+    public static void Awake1(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
             Movement.Head.Left();
             Thread.Sleep(1000);
             Movement.Head.Center();
             Thread.Sleep(1000);
             Movement.Head.Right();
-            Thread.Sleep(1000);
+            Thread.Sleep(50);
             Movement.Head.Center();
             Thread.Sleep(1000);
             Movement.Head.Release();
